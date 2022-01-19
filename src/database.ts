fe42971b676eff8b521d0293a1e868f33d704264
Davi Batista <davidedeus28@hotmail.com>
2022-01-19 17:57:14 -0300

    Mudando e conectando no banco de dados

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,14 @@
 import { createConnection, Connection } from "typeorm";
-import { User } from "./user"
+//import { User } from "./user"
 
 const connection = async () => {
   await createConnection({
     type: "mysql",
     host: "localhost",
     port: 5432,
-    username: "davi",
-    password: "davi",
-    database: "my_project"
+    username: "postgres",
+    password: "postgres",
+    database: "users"
   });
 }
 
