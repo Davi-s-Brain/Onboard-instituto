bf9bb02295476bc0353255905759d42e27a35df1
Davi Batista <davidedeus28@hotmail.com>
2022-01-21 11:27:39 -0300

    Adicionando a mutation

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
-import 'reflect-metadata'
-const { ApolloServer, gql } = require('apollo-server')
+import "reflect-metadata"
+const { ApolloServer, gql } = require("apollo-server")
+
 
 const typeDefs = gql`
+
+  type User {
+    nome: String
+    email: String
+    password: String
+    birthday: String
+  }
+
+  type Mutation {
+    createUser(nome: String, email: String, birthday: String, password: String): User!
+  }
+
   type Query {
     hello: String
-  }
+    users: [User]
+  } 
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+      hello: () => "hello",
+      users: () => [
+        {_id: '1', nome:"davi", email: "davi@exemplo.com", birthday: "20-06-2541"}
+      ]
+    }
   }
-}
+
 
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(( { url }:{ url:string } ) => console.log( `🤓 Server started at ${url}` ) )
