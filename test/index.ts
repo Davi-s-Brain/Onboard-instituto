4d7df53241bba3e3f9400f21a825ffa99a828e5c
Davi Batista <davidedeus28@hotmail.com>
2022-01-26 12:26:42 -0300

    Adding the test file

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,9 @@
 const assert = require('assert')
+import { ApolloServer } from "apollo-server"
+
+before(function() {
+  const server = new ApolloServer()
+})
 
 describe('Array', function() {
   it('should return a', () => {
