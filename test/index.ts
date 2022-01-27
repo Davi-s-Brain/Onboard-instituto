12905853259d49bb28e4eb6807cd5a9d766e57eb
Davi Batista <davidedeus28@hotmail.com>
2022-01-27 10:49:22 -0300

    Adding the tests properly

--- test/index.ts
+++ test/index.ts
@@ -1,25 +1,38 @@
-const assert = require('assert')
-import { ApolloServer, gql } from "apollo-server"
+const request = require("supertest")
+const { expect } = require('chai')
+  import { ApolloServer, gql } from "apollo-server"
 
-before(function() {
+describe('Communication with the server', function() {
+  before(function() {
 
-  const typeDefs = gql `
-    type Query {
-      hello: string
+    const typeDefs = gql `
+      type Query {
+        hello: String
+      }
+    `
+    
+    const resolvers = {
+      Query: {
+        hello: () => 'Hello world'
+      }
     }
+  
+    const server = new ApolloServer({typeDefs, resolvers})
+    server.listen().then(( { url }:{ url:string } ) => console.log( `Server started at ${url} 🤓` ) )
+  })
+
+  const query = `
+    query {
+      hello
+    } 
   `
 
-  const resolvers = {
-    Query: {
-      resolved: () => {console.log('Hello')}
-    }
-  }
-
-  const server = new ApolloServer(typeDefs, resolvers)
-})
-
-describe('Array', function() {
-  it('should return a', () => {
-    console.log('hello world')
-  })
+  it('should return Hello world', async function() {
+    const expectedResponse = {"data": {"hello": "Hello world"}}
+    const response = await request('localhost:4000').post('/').send({query})
+    
+    expect(response.statusCode).to.equal(200)
+    expect(JSON.stringify(response.body)).to.equal(JSON.stringify(expectedResponse))
+    console.log(JSON.stringify(response.body))
+  })  
 })
