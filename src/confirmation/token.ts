9a8650b96ede0eb6a7338d19ddca731a21d3525d
Davi Batista <davidedeus28@hotmail.com>
2022-02-02 21:12:40 -0300

    Adding the token

---
+++ src/confirmation/token.ts
@@ -0,0 +1,31 @@
+import * as jwt from 'jsonwebtoken';
+import { CustomError } from '../error/error';
+
+export interface DataAuthentication {
+  id: number;
+  rememberMe: boolean;
+}
+
+export class Authentication {
+  public generate(input: DataAuthentication): string {
+    const token = jwt.sign(input, process.env.JWT_KEY, {
+      expiresIn: input.rememberMe
+        ? process.env.ACCESS_TOKEN_EXPIRES_IN_REMEMBER_ME
+        : process.env.ACCESS_TOKEN_EXPIRES_IN,
+    });
+    return token;
+  }
+
+  public tokenValidator(token: string) {
+    if (!token) {
+      throw new CustomError('Token not found', 400);
+    }
+
+    try {
+      jwt.verify(token, process.env.JWT_KEY);
+      return true;
+    } catch {
+      throw new CustomError('Invalid token', 400);
+    }
+  }
+}
