a6beef3ed66943cd4914db8110e967ff0de5b139
Davi Batista <davidedeus28@hotmail.com>
2022-02-06 16:55:19 -0300

    Adding fake users and a birthday generator

--- src/seed/user-seed.ts
+++ src/seed/user-seed.ts
@@ -1,18 +1,25 @@
 import { hashPassword } from '../confirmation/passwordHash';
 import { getConnection } from 'typeorm';
 import { User } from '../entity/user';
+import { faker } from '@faker-js/faker';
 
 export async function databaseSeed() {
   const userRepository = getConnection().getRepository(User);
   const hashSupervisor = new hashPassword();
 
+  const generateBirthday = () => {
+    return `${Math.floor(Math.random() * (31 - 1) + 1)}-${Math.floor(Math.random() * (12 - 1) + 1)}-${Math.floor(
+      Math.random() * (2022 - 1500) + 1500,
+    )}`;
+  };
+
   for (let i = 1; i <= 30; i++) {
     const newUser = Object.assign(new User(), {
-      name: `fake user`,
-      email: `carlos${i}@coisa.com`,
-      birthday: `${i}0-01-20${i}0`,
-      password: await hashSupervisor.hash('babata123'),
-    })
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      email: faker.internet.email(),
+      birthday: `${generateBirthday()}`, 
+      password: await hashSupervisor.hash(faker.internet.password()),
+    });
     await userRepository.save(newUser);
   }
   try {
